0fb38a8cf731c5adebd429b25e56d7b126997552
cghil <cghilarducci@gmail.com>
2015-10-22 15:03:20 -0700

    adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ devise_for :users
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show]
+      resources :users, :only => [:show, :create]
     end
   end
 end
