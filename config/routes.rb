1242cd87a2e879758232f2ebfb8e3e33c52f311f
cghil <cghilarducci@gmail.com>
2015-10-23 16:50:16 -0700

    adds sessions controller create action and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ devise_for :users
     namespace :v1 do
       # We are going to list our resources here
       resources :users, :only => [:show, :create, :update, :destroy]
+      resources :sessions, :only => [:create, :destroy]
     end
   end
 end
