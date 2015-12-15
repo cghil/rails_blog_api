48ac9f2929b8ea9628acf8c90425f55327fde51b
cghil <cghilarducci@gmail.com>
2015-12-14 16:12:19 -0800

    created controller for Answer - answer#index - answer#create - answer#destroy
    created routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214223443) do
+ActiveRecord::Schema.define(version: 20151214233608) do
 
   create_table "answers", force: true do |t|
     t.integer  "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151214223443) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
